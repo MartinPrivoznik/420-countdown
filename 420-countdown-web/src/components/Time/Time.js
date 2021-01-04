71b9d0d588d168a1315ba52040c50c5269ee4c9f
Dohny42 <dohny42@gmail.com>
2021-01-04 02:59:57 +0100

    Additional Commit

--- 420-countdown-web/src/components/Time/Time.js
+++ 420-countdown-web/src/components/Time/Time.js
@@ -1,17 +1,19 @@
 import React from "react";
-import cls from "./Time.module.css";
+import "./styles.css";
 import TimeModel from "../../models/TimeModel";
 
 const { useState, useEffect } = React;
 
 const Time = () => {
-  const [countdownDate, setCountdownDate] = useState(TimeModel.getCountdownDateTime());
+  const [countdownDate, setCountdownDate] = useState(
+    TimeModel.getCountdownDateTime()
+  );
 
   const [state, setState] = useState({
     days: 0,
     hours: 0,
     minutes: 0,
-    seconds: 0
+    seconds: 0,
   });
 
   useEffect(() => {
@@ -28,9 +30,12 @@ const Time = () => {
       const distanceToDate = countdownDate - currentTime;
 
       // Calculate days, hours, minutes and seconds remaining
-      let days = Math.floor(distanceToDate / (1000 * 60 * 60 * 24));
-      let hours = Math.floor((distanceToDate % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-      let minutes = Math.floor((distanceToDate % (1000 * 60 * 60)) / (1000 * 60));
+      let hours = Math.floor(
+        (distanceToDate % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+      );
+      let minutes = Math.floor(
+        (distanceToDate % (1000 * 60 * 60)) / (1000 * 60)
+      );
       let seconds = Math.floor((distanceToDate % (1000 * 60)) / 1000);
 
       // For visual appeal, add a zero to each number that's only one digit
@@ -45,38 +50,16 @@ const Time = () => {
       }
 
       // Set the state to each new time
-      setState({ days: days, hours: hours, minutes, seconds });
+      setState({ hours: hours, minutes: minutes, seconds: seconds });
     }
   };
 
   return (
     <div>
-      <div className="countdown-wrapper">
-        <div className="time-section">
-          <div className="time">{state.days || "0"}</div>
-          <small className="time-text">Days</small>
-        </div>
-        <div className="time-section">
-          <div className="time">:</div>
-        </div>
-        <div className="time-section">
-          <div className="time">{state.hours || "00"}</div>
-          <small className="time-text">Hours</small>
-        </div>
-        <div className="time-section">
-          <div className="time">:</div>
-        </div>
-        <div className="time-section">
-          <div className="time">{state.minutes || "00"}</div>
-          <small className="time-text">Minutes</small>
-        </div>
-        <div className="time-section">
-          <div className="time">:</div>
-        </div>
-        <div className="time-section">
-          <div className="time">{state.seconds || "00"}</div>
-          <small className="time-text">Seconds</small>
-        </div>
+      <div className="countdown-container">
+        <span className="time">{state.hours + ":"}</span>
+        <span className="time">{state.minutes + ":"}</span>
+        <span className="time">{state.seconds}</span>
       </div>
     </div>
   );
