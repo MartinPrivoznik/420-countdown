06c0dd7f83607aed57347a9f2a7ccac6897108a7
Martin Přívozník <46354689+MartinPrivoznik@users.noreply.github.com>
2021-01-04 04:08:03 +0100

    Prepared top bar component

--- 420-countdown-web/src/App.js
+++ 420-countdown-web/src/App.js
@@ -1,4 +1,5 @@
 import Time from "./components/Time/Time";
+import TopBar from "./components/TopBar/TopBar";
 import leaf from "./images/leaf.png";
 import "./App.css";
 
@@ -6,6 +7,7 @@ const App = () => {
   return (
     <div className="App">
       <img src={leaf} className="Leaf" />
+      <TopBar />
       <Time />
     </div>
   );
---
+++ 420-countdown-web/src/components/TopBar/TopBar.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const { useState, useEffect } = React;
+
+const TopBar = () => {
+  const [countdownDate, setCountdownDate] = useState(null);
+
+  useEffect(() => {}, []);
+
+  return <div></div>;
+};
+
+export default TopBar;
