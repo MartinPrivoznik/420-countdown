be9c2fef7698da1ad14f6d80ea19e8e0aba328dc
Martin Přívozník <46354689+MartinPrivoznik@users.noreply.github.com>
2021-01-05 04:01:46 +0100

    Added info
    todo - add placeholder text

--- 420-countdown-web/src/components/TopBar/TopBar.js
+++ 420-countdown-web/src/components/TopBar/TopBar.js
@@ -1,6 +1,8 @@
 import React from "react";
 import "./TopBar.css";
+import ReactTooltip from "react-tooltip";
 import GitHubButton from "react-github-btn";
+import infoIcon from "../../images/info-icon.png";
 
 const { useState, useEffect } = React;
 
@@ -11,7 +13,11 @@ const TopBar = () => {
 
   return (
     <div className="top-bar">
-      <p>420 Countdown</p>
+      <p>420 Countdown - BETA</p>
+      <div className="top-bar-info">
+        <img src={infoIcon} data-tip="hello world" height="30" width="30" />
+        <ReactTooltip />
+      </div>
       <div className="top-bar-git">
         <GitHubButton
           href="https://github.com/MartinPrivoznik/420-countdown/tree/main/420-countdown-web"
