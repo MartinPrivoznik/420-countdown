0f850775d97289124fa56fbbc033d133bd09f7dd
Martin Přívozník <46354689+MartinPrivoznik@users.noreply.github.com>
2021-01-11 16:21:26 +0100

    Meta optimization

--- 420-countdown-web/src/pages/index.js
+++ 420-countdown-web/src/pages/index.js
@@ -1,13 +1,27 @@
 import React from "react";
+import Head from "next/head";
 import App from "../App";
 
 import { store } from "../store/store";
 import { Provider } from "react-redux";
 
 const Index = () => (
-  <Provider store={store}>
-    <App />
-  </Provider>
+  <div>
+    <Head>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta name="theme-color" content="#000000" />
+      <meta
+        name="description"
+        content="Countdown clock displaying the remaining time until upcoming 4:20. The goal is to make this a perfect place for waiting until next 420."
+      />
+      <meta name="google" content="notranslate" />
+      <meta name="google-site-verification" content="Wo-hAYg20yw3v_YVaHuG-Ywq9UHnzePgeHjQyormbSU" />
+      <title>420 Countdown</title>
+    </Head>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </div>
 );
 
 export default Index;
