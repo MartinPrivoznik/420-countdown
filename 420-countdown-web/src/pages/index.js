c67cb09bfd9b87d33af90eddd19adbb040249a2c
Martin Přívozník <46354689+MartinPrivoznik@users.noreply.github.com>
2021-01-11 16:20:29 +0100

    Revert "Moved images to static"
    This reverts commit bd51ebc35a8caa049d18ca11a3d3204ddafa90d6.

--- 420-countdown-web/src/pages/index.js
+++ 420-countdown-web/src/pages/index.js
@@ -1,29 +1,13 @@
 import React from "react";
-import Head from "next/head";
 import App from "../App";
 
 import { store } from "../store/store";
 import { Provider } from "react-redux";
 
 const Index = () => (
-  <div>
-    <Head>
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <meta name="theme-color" content="#000000" />
-      <meta
-        name="description"
-        content="Countdown clock displaying the remaining time until upcoming 4:20. The goal is to make this a perfect place for waiting until next 420."
-      />
-      <meta name="google" content="notranslate" />
-      <meta name="google-site-verification" content="Wo-hAYg20yw3v_YVaHuG-Ywq9UHnzePgeHjQyormbSU" />
-      <link rel="icon" href="/static/img/favicon.ico" />
-      <link rel="apple-touch-icon" href="/static/images/logo192.png" />
-      <title>420 Countdown</title>
-    </Head>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </div>
+  <Provider store={store}>
+    <App />
+  </Provider>
 );
 
 export default Index;
