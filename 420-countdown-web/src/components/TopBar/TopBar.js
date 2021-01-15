9b60f5c27a0f4d222bb09542d0d52dc9ea27e245
Martin Přívozník <46354689+MartinPrivoznik@users.noreply.github.com>
2021-01-15 19:15:30 +0100

    Added internal link

--- 420-countdown-web/src/components/TopBar/TopBar.js
+++ 420-countdown-web/src/components/TopBar/TopBar.js
@@ -18,7 +18,10 @@ const TopBar = () => {
 
   return (
     <div className={cls["top-bar"]}>
-      <h1>420 Countdown - BETA</h1>
+      <h1>
+        <a href="#"> 420 Countdown - BETA </a>
+      </h1>
+
       <InfoTooltip />
       <div className={cls["top-bar-switch-container"]}>
         <p>AM</p>
