bddd62bc76edd5e81bdfa89478ad4bfdb1e67c25
Martin Přívozník <46354689+MartinPrivoznik@users.noreply.github.com>
2021-01-18 11:31:46 +0100

    Added share buttons

--- 420-countdown-web/src/pages/_document.js
+++ 420-countdown-web/src/pages/_document.js
@@ -58,6 +58,7 @@ class MyDocument extends Document {
         <body>
           <Main />
           <NextScript />
+          <script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-6005614294cec8ad"></script>
         </body>
       </Html>
     );
