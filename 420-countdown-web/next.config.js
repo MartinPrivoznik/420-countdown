c9b6b061e40c66fd5b3ea1593179e5fdb7242229
Martin Přívozník <prim14@vse.cz>
2025-03-26 13:25:34 +0100

    Update next.config.js

--- 420-countdown-web/next.config.js
+++ 420-countdown-web/next.config.js
@@ -1,15 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  async redirects() {
-    return [
-      {
-        source: "/:path*",
-        has: [{ type: "host", value: "420-countdown.privoznik.dev" }],
-        destination: "https://420-countdown.com/:path*",
-        permanent: true,
-      },
-    ];
-  },
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
